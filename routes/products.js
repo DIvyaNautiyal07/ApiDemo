647f6539f5cab0ceabcdef980081808f82c12433
Divya Nautiyal <divyanautiyal07@gmail.com>
2023-09-03 22:04:11 +0530

    added logic for authorization and database

--- routes/products.js
+++ routes/products.js
@@ -1,68 +1,65 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 
-const products = [
-  {
-    name: "Apple",
-    price: 100,
-    category: "Fruits",
-    id: 1,
-  },
-  {
-    name: "Banana",
-    price: 50,
-    category: "Fruits",
-    id: 2,
-  },
-  {
-    name: "Tomato",
-    price: 120,
-    category: "Vegetables",
-    id: 3,
-  },
-];
+const Product = require("../models/products");
 
-router.get("/products", (req, res) => {
+const verifyJWT = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    res.send;
+  }
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = decoded;
+  next();
+};
+
+router.use(verifyJWT);
+
+router.get("/", async (req, res) => {
+  const products = await Product.find();
   res.send(products);
 });
 
-router.get("/products/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   const id = req.params.id;
-  const product = products.find((product) => product.id === parseInt(id));
-  res.send(product);
-});
-
-router.post("/products", (req, res) => {
-  const id = products.length + 1;
-  const product = {
-    name: req.body.name,
-    price: req.body.price,
-    category: req.body.category,
-    id: id,
-  };
-
-  products.push(product);
-  res.send({ success: true });
-});
-
-router.put("/products/:id", (req, res) => {
-  const id = req.params.id;
-  const product = req.body;
-  const dbProduct = products[parseInt(id) - 1];
-  products[parseInt(id) - 1] = Object.assign(dbProduct, product);
-  res.send({ success: true });
-});
-
-router.delete("/products/:id", (req, res) => {
-  const id = req.params.id;
-  index = products.findIndex((product) => product.id === parseInt(id));
-  console.log(index);
-  if (index < products.length && index >= 0) {
-    products.splice(index, 1);
-    res.send({ success: true });
-  } else {
-    res.send({ success: false });
+  try {
+    const product = await Product.findById(id);
+    res.send(product);
+  } catch (err) {
+    res.status(400).send({ error: "Resource Not Found" });
   }
 });
+
+router.post("/", async (req, res) => {
+  const user = req.user;
+  if (user && user.role === "ADMIN") {
+    const product = req.body;
+    const dbProduct = await Product.create(product);
+    res.send(dbProduct);
+  } else {
+    res.status(403).send({ error: "Not authorized" });
+  }
+});
+
+// router.put("/:id", (req, res) => {
+//   const id = req.params.id;
+//   const product = req.body;
+//   const dbProduct = products[parseInt(id) - 1];
+//   products[parseInt(id) - 1] = Object.assign(dbProduct, product);
+//   res.send({ success: true });
+// });
+
+// router.delete("/:id", (req, res) => {
+//   const id = req.params.id;
+//   index = products.findIndex((product) => product.id === parseInt(id));
+//   console.log(index);
+//   if (index < products.length && index >= 0) {
+//     products.splice(index, 1);
+//     res.send({ success: true });
+//   } else {
+//     res.send({ success: false });
+//   }
+// });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
 const productRouter = require("./routes/products");
+const userRouter = require("./routes/user.js");
+const mongoose = require("mongoose");
 
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log("Mongodb connected");
+});
 app.use(express.json());
 
 const logger = (req, res, next) => {
@@ -13,7 +18,8 @@ const logger = (req, res, next) => {
 
 app.use(logger);
 
-app.use(productRouter);
+app.use("/products", productRouter);
+app.use("/users", userRouter);
 
 app.listen(PORT, () => {
   console.log("server is running");
